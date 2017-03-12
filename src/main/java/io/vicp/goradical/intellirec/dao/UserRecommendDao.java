11758e8d18bd360cffcffb6c4258d2556eb3ab12
Kenny <kenny__jing@163.com>
2017-03-12 11:57:24 +0800

    UserRecordDao方法定义及其实现

--- src/main/java/io/vicp/goradical/intellirec/dao/UserRecommendDao.java
+++ src/main/java/io/vicp/goradical/intellirec/dao/UserRecommendDao.java
@@ -2,9 +2,17 @@ package io.vicp.goradical.intellirec.dao;
 
 import io.vicp.goradical.intellirec.model.pmrs.UserRecommend;
 
+import java.io.Serializable;
+
 /**
  * Created by Kenny on 2017/3/6.
  */
 public interface UserRecommendDao extends BaseDao<UserRecommend> {
 
+	/**
+	 * 根据视频id统计视频总的 推荐/不推荐 数量
+	 * @param videoId 视频id
+	 * @return 统计总数
+	 */
+	long countTotalUserRecommendWithVideoId(Serializable videoId);
 }
--- src/main/java/io/vicp/goradical/intellirec/dao/impl/UserRecommendDaoImpl.java
+++ src/main/java/io/vicp/goradical/intellirec/dao/impl/UserRecommendDaoImpl.java
@@ -4,10 +4,21 @@ import io.vicp.goradical.intellirec.dao.UserRecommendDao;
 import io.vicp.goradical.intellirec.model.pmrs.UserRecommend;
 import org.springframework.stereotype.Repository;
 
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by Kenny on 2017/3/6.
  */
 @Repository("userRecommendDao")
 public class UserRecommendDaoImpl extends BaseDaoImpl<UserRecommend> implements UserRecommendDao {
 
+	@Override
+	public long countTotalUserRecommendWithVideoId(Serializable videoId) {
+		String hql = "select count(*) from UserRecommend cr where cr.video.id = :videoId";
+		Map<String, Object> params = new HashMap<>();
+		params.put("videoId", videoId);
+		return (long) uniqueResult(hql, params);
+	}
 }
