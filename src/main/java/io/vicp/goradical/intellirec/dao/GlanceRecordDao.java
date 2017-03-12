db69003b75428da98443999210756ef9ea1c29f5
Kenny <kenny__jing@163.com>
2017-03-12 11:53:16 +0800

    GlanceRecordDao方法定义及其实现

--- src/main/java/io/vicp/goradical/intellirec/dao/GlanceRecordDao.java
+++ src/main/java/io/vicp/goradical/intellirec/dao/GlanceRecordDao.java
@@ -2,9 +2,16 @@ package io.vicp.goradical.intellirec.dao;
 
 import io.vicp.goradical.intellirec.model.pmrs.GlanceRecord;
 
+import java.io.Serializable;
+
 /**
  * Created by Kenny on 2017/3/6.
  */
 public interface GlanceRecordDao extends BaseDao<GlanceRecord> {
-
+	/**
+	 * 根据视频id统计视频总的浏览量
+	 * @param videoId 视频id
+	 * @return 统计总数
+	 */
+	long countTotalGlanceWithVideoId(Serializable videoId);
 }
--- src/main/java/io/vicp/goradical/intellirec/dao/impl/GlanceRecordDaoImpl.java
+++ src/main/java/io/vicp/goradical/intellirec/dao/impl/GlanceRecordDaoImpl.java
@@ -4,10 +4,20 @@ import io.vicp.goradical.intellirec.dao.GlanceRecordDao;
 import io.vicp.goradical.intellirec.model.pmrs.GlanceRecord;
 import org.springframework.stereotype.Repository;
 
+import java.io.Serializable;
+import java.util.HashMap;
+
 /**
  * Created by Kenny on 2017/3/6.
  */
 @Repository("glanceRecordDao")
 public class GlanceRecordDaoImpl extends BaseDaoImpl<GlanceRecord> implements GlanceRecordDao {
 
+	@Override
+	public long countTotalGlanceWithVideoId(Serializable videoId) {
+		String hql = "select count(*) from GlanceRecord gr where gr.video.id = :videoId";
+		HashMap<String, Object> params = new HashMap<>();
+		params.put("videoId", videoId);
+		return (long) uniqueResult(hql, params);
+	}
 }
