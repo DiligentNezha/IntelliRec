2fd330f21f6538c1841a36a159d4209c20e19247
Kenny <kenny__jing@163.com>
2017-03-12 11:56:09 +0800

    PraiseRecordDao方法定义及其实现

--- src/main/java/io/vicp/goradical/intellirec/dao/PraiseRecordDao.java
+++ src/main/java/io/vicp/goradical/intellirec/dao/PraiseRecordDao.java
@@ -2,9 +2,16 @@ package io.vicp.goradical.intellirec.dao;
 
 import io.vicp.goradical.intellirec.model.pmrs.PraiseRecord;
 
+import java.io.Serializable;
+
 /**
  * Created by Kenny on 2017/3/6.
  */
 public interface PraiseRecordDao extends BaseDao<PraiseRecord> {
-
+	/**
+	 * 根据视频id统计视频总的 赞/踩 数量
+	 * @param videoId 视频id
+	 * @return 统计总数
+	 */
+	long countTotalPraiseWithVideoId(Serializable videoId);
 }
--- src/main/java/io/vicp/goradical/intellirec/dao/impl/PraiseRecordDaoImpl.java
+++ src/main/java/io/vicp/goradical/intellirec/dao/impl/PraiseRecordDaoImpl.java
@@ -4,10 +4,21 @@ import io.vicp.goradical.intellirec.dao.PraiseRecordDao;
 import io.vicp.goradical.intellirec.model.pmrs.PraiseRecord;
 import org.springframework.stereotype.Repository;
 
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by Kenny on 2017/3/6.
  */
 @Repository("praiseRecordDao")
 public class PraiseRecordDaoImpl extends BaseDaoImpl<PraiseRecord> implements PraiseRecordDao {
 
+	@Override
+	public long countTotalPraiseWithVideoId(Serializable videoId) {
+		String hql = "select count(*) from PraiseRecord cr where cr.video.id = :videoId";
+		Map<String, Object> params = new HashMap<>();
+		params.put("videoId", videoId);
+		return (long) uniqueResult(hql, params);
+	}
 }
