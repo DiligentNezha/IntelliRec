c042954a88b60b4350bdbcde3fd225f5e459af16
Kenny <kenny__jing@163.com>
2017-03-12 11:54:08 +0800

    MarkRecordDao方法定义及其实现

--- src/main/java/io/vicp/goradical/intellirec/dao/MarkRecordDao.java
+++ src/main/java/io/vicp/goradical/intellirec/dao/MarkRecordDao.java
@@ -2,9 +2,17 @@ package io.vicp.goradical.intellirec.dao;
 
 import io.vicp.goradical.intellirec.model.pmrs.MarkRecord;
 
+import java.io.Serializable;
+
 /**
  * Created by Kenny on 2017/3/6.
  */
 public interface MarkRecordDao extends BaseDao<MarkRecord> {
 
+	/**
+	 * 根据视频id统计视频总的评分量
+	 * @param videoId 视频id
+	 * @return 统计总数
+	 */
+	long countTotalMarkWithVideoId(Serializable videoId);
 }
--- src/main/java/io/vicp/goradical/intellirec/dao/impl/MarkRecordDaoImpl.java
+++ src/main/java/io/vicp/goradical/intellirec/dao/impl/MarkRecordDaoImpl.java
@@ -4,10 +4,21 @@ import io.vicp.goradical.intellirec.dao.MarkRecordDao;
 import io.vicp.goradical.intellirec.model.pmrs.MarkRecord;
 import org.springframework.stereotype.Repository;
 
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by Kenny on 2017/3/6.
  */
 @Repository("markRecordDao")
 public class MarkRecordDaoImpl extends BaseDaoImpl<MarkRecord> implements MarkRecordDao{
 
+	@Override
+	public long countTotalMarkWithVideoId(Serializable videoId) {
+		String hql = "select count(*) from MarkRecord cr where cr.video.id = :videoId";
+		Map<String, Object> params = new HashMap<>();
+		params.put("videoId", videoId);
+		return (long) uniqueResult(hql, params);
+	}
 }
