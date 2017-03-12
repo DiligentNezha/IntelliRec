41150cffc20dc42f0dc05c92a3ada70ec6b6c9ff
Kenny <kenny__jing@163.com>
2017-03-12 11:51:32 +0800

    CollectionRecordDao方法定义及其实现

--- src/main/java/io/vicp/goradical/intellirec/dao/impl/CollectionRecordDaoImpl.java
+++ src/main/java/io/vicp/goradical/intellirec/dao/impl/CollectionRecordDaoImpl.java
@@ -5,10 +5,21 @@ import io.vicp.goradical.intellirec.dao.CollectionRecordDao;
 import io.vicp.goradical.intellirec.model.pmrs.CollectRecord;
 import org.springframework.stereotype.Repository;
 
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by Kenny on 2017/3/6.
  */
 @Repository("collectionRecordDao")
 public class CollectionRecordDaoImpl extends BaseDaoImpl<CollectRecord> implements CollectionRecordDao {
 
+	@Override
+	public long countTotalCollectWithVideoId(Serializable videoId) {
+		String hql = "select count(*) from CollectRecord cr where cr.video.id = :videoId";
+		Map<String, Object> params = new HashMap();
+		params.put("videoId", videoId);
+		return (long) uniqueResult(hql, params);
+	}
 }
