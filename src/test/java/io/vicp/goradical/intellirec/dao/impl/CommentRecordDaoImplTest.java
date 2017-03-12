746d7ff9969decd6abd003772f57e04445184322
Kenny <kenny__jing@163.com>
2017-03-12 11:59:37 +0800

    CommentRecordDao方法测试

--- src/test/java/io/vicp/goradical/intellirec/dao/impl/CommentRecordDaoImplTest.java
+++ src/test/java/io/vicp/goradical/intellirec/dao/impl/CommentRecordDaoImplTest.java
@@ -10,13 +10,16 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.text.SimpleDateFormat;
+import java.util.List;
+import java.util.Set;
+
 /**
  * Created by Kenny on 2017/3/8.
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = "classpath:applicationContext.xml")
 public class CommentRecordDaoImplTest extends AbstractTransactionalJUnit4SpringContextTests {
-
 	private static final Logger LOG = LogManager.getLogger(CommentRecordDaoImplTest.class);
 
 	@Autowired
@@ -25,9 +28,40 @@ public class CommentRecordDaoImplTest extends AbstractTransactionalJUnit4SpringC
 	@Test
 	public void countTotalPlay() throws Exception {
 		for (int i = 1; i < 3000 ; i++) {
-			long count = crd.countTotalPlayWithVideoId(i);
+			long count = crd.countTotalCommentWithVideoId(i);
 			LOG.info(count);
 		}
 	}
 
+	@Test
+	public void getUserIdsWithVideoId() throws Exception {
+		Set userIdsWithVideoId = crd.getUserIdsWithVideoId(1);
+		LOG.info(userIdsWithVideoId.size());
+	}
+
+	@Test
+	public void getCommentRecordList() throws Exception {
+		List crl = crd.getCommentRecordList(1);
+		for (int i = 0; i < crl.size(); i++) {
+			Object[] record = (Object[]) crl.get(i);
+			StringBuilder sb = new StringBuilder();
+			for (int j = 0; j < record.length; j++) {
+				sb.append(record[j] + " ");
+			}
+			LOG.info(sb);
+		}
+	}
+
+	@Test
+	public void getCommentRecordList1() throws Exception {
+		List crl = crd.getCommentRecordList(new SimpleDateFormat("yyyy-MM-dd").parse("2015-06-22"));
+		for (int i = 0; i < crl.size(); i++) {
+			Object[] record = (Object[]) crl.get(i);
+			StringBuilder sb = new StringBuilder();
+			for (int j = 0; j < record.length; j++) {
+				sb.append(record[j] + " ");
+			}
+			LOG.info(sb);
+		}
+	}
 }
