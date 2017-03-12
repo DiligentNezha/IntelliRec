2cf36ab1318a9661a0eed4f398df5398f7f47c81
Kenny <kenny__jing@163.com>
2017-03-12 11:55:03 +0800

    PlayRecordDao方法定义及其实现

--- src/main/java/io/vicp/goradical/intellirec/dao/PlayRecordDao.java
+++ src/main/java/io/vicp/goradical/intellirec/dao/PlayRecordDao.java
@@ -2,9 +2,16 @@ package io.vicp.goradical.intellirec.dao;
 
 import io.vicp.goradical.intellirec.model.pmrs.PlayRecord;
 
+import java.io.Serializable;
+
 /**
  * Created by Kenny on 2017/3/6.
  */
 public interface PlayRecordDao extends BaseDao<PlayRecord> {
-
+	/**
+	 * 根据视频id统计视频总的播放量
+	 * @param videoId 视频id
+	 * @return 统计总数
+	 */
+	long countTotalPlayWithVideoId(Serializable videoId);
 }
--- src/main/java/io/vicp/goradical/intellirec/dao/impl/PlayRecordDaoImpl.java
+++ src/main/java/io/vicp/goradical/intellirec/dao/impl/PlayRecordDaoImpl.java
@@ -4,10 +4,20 @@ import io.vicp.goradical.intellirec.dao.PlayRecordDao;
 import io.vicp.goradical.intellirec.model.pmrs.PlayRecord;
 import org.springframework.stereotype.Repository;
 
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by Kenny on 2017/3/6.
  */
 @Repository("playRecordDao")
 public class PlayRecordDaoImpl extends BaseDaoImpl<PlayRecord> implements PlayRecordDao {
-
+	@Override
+	public long countTotalPlayWithVideoId(Serializable videoId) {
+		String hql = "select count(*) from PlayRecord cr where cr.video.id = :videoId";
+		Map<String, Object> params = new HashMap<>();
+		params.put("videoId", videoId);
+		return (long) uniqueResult(hql, params);
+	}
 }
