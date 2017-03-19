0fecf276abbec23d0b17c366dd18f1b78be03905
Kenny <kenny__jing@163.com>
2017-03-19 09:30:33 +0800

    UserService update

--- src/main/java/io/vicp/goradical/intellirec/service/UserService.java
+++ src/main/java/io/vicp/goradical/intellirec/service/UserService.java
@@ -1,9 +1,25 @@
 package io.vicp.goradical.intellirec.service;
 
 import io.vicp.goradical.intellirec.model.pmrs.User;
+import io.vicp.goradical.intellirec.model.pmrs.vo.TableVo;
+import io.vicp.goradical.intellirec.model.pmrs.vo.UserVo;
 
 /**
  * Created by Kenny on 2017/3/6.
  */
 public interface UserService extends BaseService<User>{
+	/**
+	 * 根据用户邮箱和密码进行验证登陆
+	 * @param email 邮箱
+	 * @param password 密码
+	 * @return 用户视图模型
+	 */
+	UserVo login(String email, String password);
+
+	/**
+	 * 根据用户视图模型返回用户数据的数据表格模型
+	 * @param userVo
+	 * @return 用户数据的数据表格模型
+	 */
+	TableVo<UserVo> tableData(UserVo userVo);
 }
--- src/main/java/io/vicp/goradical/intellirec/service/impl/UserServiceImpl.java
+++ src/main/java/io/vicp/goradical/intellirec/service/impl/UserServiceImpl.java
@@ -2,9 +2,17 @@ package io.vicp.goradical.intellirec.service.impl;
 
 import io.vicp.goradical.intellirec.dao.UserDao;
 import io.vicp.goradical.intellirec.model.pmrs.User;
+import io.vicp.goradical.intellirec.model.pmrs.vo.TableVo;
+import io.vicp.goradical.intellirec.model.pmrs.vo.UserVo;
 import io.vicp.goradical.intellirec.service.UserService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Kenny on 2017/3/6.
@@ -14,5 +22,54 @@ public class UserServiceImpl extends BaseServiceImpl<User> implements UserServic
 
 	@Autowired
 	private UserDao userDao;
+
+	@Override
+	public UserVo login(String email, String password) {
+		User user = userDao.login(email, password);
+		UserVo userVo = null;
+		if (user != null) {
+			userVo = new UserVo();
+			BeanUtils.copyProperties(user, userVo);
+		}
+		return userVo;
+	}
+
+	@Override
+	public TableVo<UserVo> tableData(UserVo userVo) {
+		TableVo<UserVo> userVoTableVo = new TableVo<>();
+		String hql = "from User u";
+		HashMap<String, Object> params = new HashMap<>();
+		hql = addWhere(userVo, hql, params);
+		userDao.getCurrentSession().createQuery(hql);
+		String countHql = "select count(*) " + hql;
+		List<User> userList = userDao.findEntityByHQL(hql, params, userVo.getPage(), userVo.getRows());
+		List<UserVo> userVoList = new ArrayList<>(userList.size());
+		for (User user : userList) {
+			UserVo userVoTemp = new UserVo();
+			userVoTemp.setPage(userVo.getPage());
+			userVoTemp.setRows(userVo.getRows());
+			BeanUtils.copyProperties(user, userVoTemp);
+			userVoList.add(userVoTemp);
+		}
+		userVoTableVo.setRecordsTotal((Long) userDao.uniqueResult(countHql, params));
+		userVoTableVo.setRows(userVoList);
+		return userVoTableVo;
+	}
+
+	private String addWhere(UserVo userVo, String hql, Map<String, Object> params) {
+		if (userVo.getUserName() != null && !"".equals(userVo.getUserName().trim())) {
+			hql += " where u.userName like :name";
+			params.put("name", "%%" + userVo.getUserName().trim() + "%%");
+		}
+		if (userVo.getEmail() != null && !"".equals(userVo.getEmail().trim())) {
+			if (userVo.getUserName() == null || "".equals((userVo.getUserName().trim()))) {
+				hql += " where u.email like :email";
+			} else {
+				hql += " and u.email like :email";
+			}
+			params.put("email", "%%" + userVo.getEmail().trim() + "%%");
+		}
+		return hql;
+	}
 
 }
