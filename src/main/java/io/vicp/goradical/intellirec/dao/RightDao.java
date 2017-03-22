ddd730edeb39ba9100af14ec7f57052acefc2a8b
Kenny <kenny__jing@163.com>
2017-03-22 20:08:22 +0800

    添加RightDao及其实现类

---
+++ src/main/java/io/vicp/goradical/intellirec/dao/RightDao.java
@@ -0,0 +1,9 @@
+package io.vicp.goradical.intellirec.dao;
+
+import io.vicp.goradical.intellirec.model.security.Right;
+
+/**
+ * Created by Kenny on 2017/3/22.
+ */
+public interface RightDao extends BaseDao<Right> {
+}
---
+++ src/main/java/io/vicp/goradical/intellirec/dao/impl/RightDaoImpl.java
@@ -0,0 +1,13 @@
+package io.vicp.goradical.intellirec.dao.impl;
+
+import io.vicp.goradical.intellirec.dao.RightDao;
+import io.vicp.goradical.intellirec.model.security.Right;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by Kenny on 2017/3/6.
+ */
+@Repository("rightDao")
+public class RightDaoImpl extends BaseDaoImpl<Right> implements RightDao {
+
+}
