c5ecfb49eddb6e6dd9ef326c7bdc1c336d7e0f87
Kenny <kenny__jing@163.com>
2017-03-26 08:53:40 +0800

    add RoleService and sub

---
+++ src/main/java/io/vicp/goradical/intellirec/service/RoleService.java
@@ -0,0 +1,10 @@
+package io.vicp.goradical.intellirec.service;
+
+import io.vicp.goradical.intellirec.model.security.Role;
+
+/**
+ * Created by Kenny on 2017/3/6.
+ */
+public interface RoleService extends BaseService<Role>{
+
+}
