25273acf8ef54790a92d844e18c8ea52da45ce44
Kenny <kenny__jing@163.com>
2017-03-29 18:53:46 +0800

    add VideoStaticsPieVo

--- src/main/java/io/vicp/goradical/intellirec/model/echartsvo/VideoStatisticsPieVo.java
+++ src/main/java/io/vicp/goradical/intellirec/model/echartsvo/VideoStatisticsPieVo.java
@@ -1,7 +1,47 @@
 package io.vicp.goradical.intellirec.model.echartsvo;
 
+import java.util.List;
+import java.util.Map;
+
 /**
  * Created by Kenny on 2017/3/29.
  */
 public class VideoStatisticsPieVo {
+
+	private int id;
+	private String videoName;
+	private List<Map<String, Object>> seriesData;
+	private List<Object> legendData;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getVideoName() {
+		return videoName;
+	}
+
+	public void setVideoName(String videoName) {
+		this.videoName = videoName;
+	}
+
+	public List<Map<String, Object>> getSeriesData() {
+		return seriesData;
+	}
+
+	public void setSeriesData(List<Map<String, Object>> seriesData) {
+		this.seriesData = seriesData;
+	}
+
+	public List<Object> getLegendData() {
+		return legendData;
+	}
+
+	public void setLegendData(List<Object> legendData) {
+		this.legendData = legendData;
+	}
 }
